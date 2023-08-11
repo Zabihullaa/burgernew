31c449896200683a279fac4b9d6db377b3472a30
Zabihulla <111756274+Zabihullaa@users.noreply.github.com>
2023-08-11 18:22:18 +0500

    Add files via upload

---
+++ Js/js.js
@@ -0,0 +1,8 @@
+// const navBtn = document.querySelector('.nav__btn'),
+// navList = document.querySelector('.nav__list'),
+// navClose = document.querySelector('.nav__close');
+
+
+// navBtn.addEventListener('click', function () {
+//     navListstyle.transform = "translateY(100%)"
+// })
